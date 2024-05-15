2c38727cbd13c367a00028c5c513a298ae7263db
dawidseipold <seipold.dawid@gmail.com>
2024-05-15 21:11:21 +0200

    WEBAPP: build: setup tanstack query and router build: setup tailwind build: add tanstack form and table for future use build: add tailwind utility libraries feat: add cn function for tailwind classes feat: add src alias for the project chore: remove unnecessary files

--- apps/webapp/src/main.tsx
+++ apps/webapp/src/main.tsx
@@ -1,10 +1,30 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from "react";
+import ReactDOM from "react-dom/client";
+import "@/styles/index.css";
+import { RouterProvider, createRouter } from "@tanstack/react-router";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import { routeTree } from "./routeTree.gen";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const router = createRouter({ routeTree });
+
+declare module "@tanstack/react-router" {
+  interface Register {
+    router: typeof router;
+  }
+}
+
+const rootElement = document.getElementById("root");
+
+if (!rootElement?.innerHTML) {
+  const root = ReactDOM.createRoot(rootElement!);
+  const queryClient = new QueryClient();
+
+  root.render(
+    <StrictMode>
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
+    </StrictMode>
+  );
+}
